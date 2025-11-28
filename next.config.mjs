ac4ef7214bdb2fb86d11595c7a0d014eaea226fe
Nguyen Quoc Hung <nguyenquochung.workvn@gmail.com>
2025-11-29 02:13:46 +0700

    feat: update cicd

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: "export",
+  basePath: "/virulen",
+  assetPrefix: "/virulen/",
   typescript: {
     ignoreBuildErrors: true,
   },
